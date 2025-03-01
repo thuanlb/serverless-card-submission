6c9df81fabfd65bc9c579d336f5dd58dfa6972e8
thuanlb <lebathuan98@gmail.com>
2025-03-01 22:38:10 +0700

    change config image

--- frontend/app/Main.tsx
+++ frontend/app/Main.tsx
@@ -175,6 +175,7 @@ export default function Main() {
                         width={40}
                         height={40}
                         className="rounded-full"
+                        unoptimized
                       />
                     </TableCell>
                     <TableCell>{entry.name}</TableCell>
